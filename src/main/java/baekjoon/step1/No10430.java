62716f7d6f25ac15a3e914f857622e83a65a3efc
gohardbro <wkd9155@gmail.com>
2022-10-05 22:24:35 +0900

    first commit

---
+++ src/main/java/baekjoon/step1/No10430.java
@@ -0,0 +1,15 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+//나머지
+//아직 안풀었음
+public class No10430 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+
+    }
+}
---
+++ src/main/java/baekjoon/step1/No18108.java
@@ -0,0 +1,13 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+public class No18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int buddaYear = scanner.nextInt();
+        int normalYear = buddaYear - 543;
+
+        System.out.println(normalYear);
+    }
+}
