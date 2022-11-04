29b3ba1e3865987ad7fc8afec6e19fae64f3b24e
gohardbro <wkd9155@gmail.com>
2022-11-04 21:20:46 +0900

    #solved 1 problem

---
+++ src/main/java/baekjoon/step11/recursive/No10870.java
@@ -0,0 +1,23 @@
+package baekjoon.step11.recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 피보나치 수 5
+public class No10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(fibonacci(num));
+    }
+
+    private static int fibonacci(int num) {
+        if (num == 0) return 0;
+        else if (num == 1) return 1;
+
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
