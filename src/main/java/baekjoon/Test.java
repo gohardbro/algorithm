cc8b1630ef0a66c2c9e5dc536f35cf78cb7e0c30
gohardbro <wkd9155@gmail.com>
2022-11-08 23:12:23 +0900

    #test

--- src/main/java/baekjoon/Test.java
+++ src/main/java/baekjoon/Test.java
@@ -1,12 +1,22 @@
 package baekjoon;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<Integer>();
 
-        String s = "abcdefg";
-        StringBuilder sb = new StringBuilder(s);
-        System.out.println(sb.reverse());
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+
+        while(!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+
     }
 }
