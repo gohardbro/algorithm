87e03a4f037c6b8cfb7fbc6f333f819f7cb30bcf
gohardbro <wkd9155@gmail.com>
2022-11-13 22:44:17 +0900

    #solved 1 problem

--- src/main/java/baekjoon/algorithm_basic1/No1158.java
+++ src/main/java/baekjoon/algorithm_basic1/No1158.java
@@ -4,11 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Objects;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 요세푸스 문제
+// 첫시도 584ms 나옴 시간 줄여보자!
 public class No1158 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,11 +29,12 @@ public class No1158 {
         for (int j = 0; j < n; j++) {
 
             for (int i = 0; i < k; i++) {
-
                 num = queue.poll();
-                if (!Objects.equals(num, k)) {
-                    queue.offer(num);
-                }
+
+                if (i==k-1)
+                    break;
+
+                queue.offer(num);
             }
 
             if (j == n - 1)
