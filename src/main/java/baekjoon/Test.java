a7e992fcd66d7286c1266f01262d64a5afc2943f
gohardbro <wkd9155@gmail.com>
2023-01-11 08:36:50 +0900

    practice 패키지 추가

--- src/main/java/baekjoon/Test.java
+++ src/main/java/baekjoon/Test.java
@@ -1,18 +1,7 @@
 package baekjoon;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Test {
     public static void main(String[] args) {
-        int start=0;
-        int end =0;
-        int[] arr = new int[9];
-        for(; start< end; start++, end--){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-        }
 
     }
 }
