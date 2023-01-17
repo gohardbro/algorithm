69db5ec6e8f74d334b1dfb7ff444c97bb51471b3
gohardbro <wkd9155@gmail.com>
2023-01-17 09:09:38 +0900

    MergeSort 연습

--- src/main/java/practice/MergeSortPractice.java
+++ src/main/java/practice/MergeSortPractice.java
@@ -4,52 +4,62 @@ import java.util.Arrays;
 
 // 병합정렬 구현 연습
 public class MergeSortPractice {
+
+    private static int[] tmp;
+
     public static void main(String[] args) {
-        int[] nums = {2,9,6,4,1,5,8,7,3};
+        int[] nums = {2, 9, 6, 4, 1, 5, 8, 7, 3};
+        tmp = new int[nums.length];
 
         System.out.println("정렬 전: " + Arrays.toString(nums));
         mergeSort(nums);
         System.out.println("정렬 후: " + Arrays.toString(nums));
-        
     }
 
     private static void mergeSort(int[] nums) {
-        int[] tmp = new int[nums.length];
-        mergeSort(nums, tmp, 0, nums.length-1);
+        mergeSort(nums, 0, nums.length - 1);
     }
 
-    private static void mergeSort(int[] nums, int[] tmp, int start, int end) {
-        if (start < end) {
-            int mid = (start + end) / 2;
-            mergeSort(nums, tmp, 0, mid);
-            mergeSort(nums, tmp, mid+1, end);
-            merge(nums, tmp, start, mid, end);
+    private static void mergeSort(int[] nums, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            int mid = (startIndex + endIndex) / 2;
+            mergeSort(nums, 0, mid); // 왼쪽
+            mergeSort(nums, mid + 1, endIndex); // 오른쪽
+            merge(nums, startIndex, mid, endIndex);
         }
     }
 
-    private static void merge(int[] nums, int[] tmp, int start, int mid, int end) {
-        for (int i = start; i <= end; i++) {
-            tmp[i] = nums[i];
-        }
+    private static void merge(int[] nums, int startIndex, int mid, int endIndex) {
+        int part1First = startIndex;
+        int part2First = mid + 1;
+        int index = startIndex;
 
-        int part1 = start;
-        int part2 = mid + 1;
-        int index = start;
-
-        while (part1 <= mid && part2 <= end) {
-            if (tmp[part1] <= tmp[part2]) {
-                nums[index] = tmp[part1];
-                part1++;
+        while (part1First <= mid && part2First <= endIndex) {
+            if (nums[part1First] <= nums[part2First]) {
+                tmp[index] = nums[part1First];
+                part1First++;
             } else {
-                nums[index] = tmp[part2];
-                part2++;
+                tmp[index] = nums[part2First];
+                part2First++;
             }
 
             index++;
         }
 
-        for (int i = 0; i <= mid-part1; i++) {
-            nums[index + i] = tmp[part1 + i];
+
+        while (part1First <= mid) {
+            tmp[index++] = nums[part1First++];
+        }
+
+        while (part2First <= endIndex) {
+            tmp[index++] = nums[part2First++];
+        }
+
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            nums[i] = tmp[i];
         }
     }
+
+
 }
