a50c31920996a44869d7ada30eab4517056d123a
gohardbro <89912930+gohardbro@users.noreply.github.com>
2023-02-15 22:15:59 +0900

    [Silver II] Title: N과 M (10), Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] nums;
+    private static int[] arr;
+    private static boolean[] isVisited;
+    private static Set<String> set = new LinkedHashSet<>();
+    
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        nums = new int[N];
+        arr = new int[M];
+        isVisited = new boolean[N];
+        
+        s = br.readLine().split(" ");
+        for (int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(s[i]);
+        }
+        
+        Arrays.sort(nums);
+        
+        sol(0, 0);
+        
+        
+        for (String str : set) {
+            sb.append(str).append("\n");
+        }
+        System.out.println(sb);
+    }
+    
+    private static void sol(int index, int depth) {
+        if (depth == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            
+            set.add(sb.toString());
+            return;
+        }
+        
+        for (int i=index; i<N; i++) {
+            if (isVisited[i]) continue;
+            
+            isVisited[i] = true;
+            arr[depth] = nums[i];
+            sol(i+1, depth+1);
+            isVisited[i] = false;
+        }
+    }
+}
