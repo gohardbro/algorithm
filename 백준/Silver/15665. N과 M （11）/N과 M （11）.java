63dd3c712e39e07e443d333a6b7f9e537f75a6cf
gohardbro <89912930+gohardbro@users.noreply.github.com>
2023-02-17 21:31:08 +0900

    [Silver II] Title: N과 M (11), Time: 1036 ms, Memory: 151408 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] nums;
+    private static int[] arr;
+    private static Set<String> set = new LinkedHashSet<>();
+    
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        nums = new int[N];
+        arr = new int[M];
+        
+        s = br.readLine().split(" ");
+        for (int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(s[i]);
+        }
+        
+        Arrays.sort(nums);
+        
+        sol(0);
+        
+        StringBuilder sb = new StringBuilder();
+        for (String str : set) {
+            sb.append(str).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+    
+    
+    private static void sol(int depth) {
+        if (depth == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            
+            set.add(sb.toString());
+            return;
+        }
+        
+        for (int i=0; i<N; i++) {
+            arr[depth] = nums[i];
+            sol(depth+1);
+        }
+    }
+}
