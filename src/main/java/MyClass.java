d0865673bd427d4c76f951ba95bc18d79aa53147
gohardbro <wkd9155@gmail.com>
2023-03-21 21:05:29 +0900

    sol 1

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,87 +1,40 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class MyClass {
-    private static int R, C;
-    private static char[][] alphabets;
-    private static boolean[] isVisited;
-    private static int max = Integer.MIN_VALUE;
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        String[] s = br.readLine().split(" ");
-        R = Integer.parseInt(s[0]);
-        C = Integer.parseInt(s[1]);
-        alphabets = new char[R][C];
-        isVisited = new boolean[26]; // 알파벳 개수 크기의 방문여부 확인배열
+        int n = Integer.parseInt(br.readLine());
+        HashMap<Integer, HashSet<String>> map = new HashMap<>();
 
-        // 입력 알파벳들 배열에 저장
-        for (int i=0; i<R; i++) {
-            String str = br.readLine();
-            for (int j=0; j<C; j++) {
-                alphabets[i][j] = str.charAt(j);
+        for (int i=0; i<n; i++) {
+            String s = br.readLine();
+
+            map.putIfAbsent(s.length(), new HashSet<>());
+            map.get(s.length()).add(s);
+            map.put(s.length(), map.get(s.length()));
+        }
+
+        Set<Integer> keySets = map.keySet();
+        int keySetsSize = keySets.size();
+        Integer[] keys = keySets.toArray(new Integer[keySetsSize]);
+        Arrays.sort(keys);
+
+        for (int key : keys) {
+            HashSet<String> strSets = map.get(key);
+            int strsLitSize = strSets.size();
+            String[] strs = strSets.toArray(new String[strsLitSize]);
+            Arrays.sort(strs);
+
+            for (String str : strs) {
+                sb.append(str).append("\n");
             }
         }
 
-        isVisited[(int) alphabets[0][0] - 'A'] = true;
-
-        sol(0, 0, 1);
-
-        System.out.println(max);
-    }
-
-    private static void sol(int row, int col, int cnt) {
-        boolean isExistNext = false; // 다음 갈수있는 칸이 있나
-
-        if (row < R-1) {
-            int index = alphabets[row+1][col] - 'A';
-
-            if (!isVisited[index]) {
-                isVisited[index] = true;
-                isExistNext = true;
-
-                sol(row+1, col, cnt+1);
-                isVisited[index] = false;
-            }
-        }
-        if (row > 0) {
-            int index = alphabets[row-1][col] - 'A';
-
-            if (!isVisited[index]) {
-                isVisited[index] = true;
-                isExistNext = true;
-
-                sol(row-1, col, cnt+1);
-                isVisited[index] = false;
-            }
-        }
-        if (col < C-1) {
-            int index = alphabets[row][col+1] - 'A';
-
-            if (!isVisited[index]) {
-                isVisited[index] = true;
-                isExistNext = true;
-
-                sol(row, col+1, cnt+1);
-                isVisited[index] = false;
-            }
-        }
-        if (col > 0) {
-            int index = alphabets[row][col-1] - 'A';
-
-            if (!isVisited[index]) {
-                isVisited[index] = true;
-                isExistNext = true;
-
-                sol(row, col-1, cnt+1);
-                isVisited[index] = false;
-            }
-        }
-
-        if (!isExistNext) {
-            if (cnt > max) max = cnt;
-            return;
-        }
-
+        System.out.println(sb);
     }
 }
