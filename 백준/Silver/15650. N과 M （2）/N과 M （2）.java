3b100378bed6add568eafa1f3a1f12930691b23a
gohardbro <89912930+gohardbro@users.noreply.github.com>
2023-09-19 19:52:23 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,57 +1,45 @@
-import java.io.*;
-// N과 M (2)
-/*
-1. 아이디어
-    - dfs로 재귀함수를 이용 백트래킹
-    - 재귀 depth == M 이면 종료
-    - 가지치기: 방문한 숫자는 탐색제외로 
-    - 결과수열이 중복 불가이기 때문에 재귀호출 logic에서 부모노드+1 숫자부터 탐색해야한다.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-2. 시간복잡도
-    - n!
-    
-3. 자료구조
-    - 결과값 저장 int[] arr
-    - 방문여부 확인 boolean[] isVisited
-*/
 public class Main {
-    private static int N, M;
-    private static int[] arr;
-    private static boolean[] isVisited;
-    private static StringBuilder sb = new StringBuilder();
-    
+    private static final StringBuilder sb = new StringBuilder();
+    private static boolean[] isVisited; // 숫자 방문여부
+    private static int[] arr; // 결과 숫자
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] s = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
-        arr = new int[M];
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
         isVisited = new boolean[N];
-        
-        dfs(0, 0);
-        
+        arr = new int[M];
+
+        sol(N, M);
+
         System.out.println(sb);
     }
-    
-    private static void dfs(int nowNum, int depth) {
-        // 종료조건
+
+    private static void sol(int N, int M) {
+        sol(N, M, 0, 0);
+    }
+
+    private static void sol(int N, int M, int depth, int index) {
         if (depth == M) {
             for (int num : arr) {
                 sb.append(num).append(" ");
             }
-            
             sb.append("\n");
             return;
         }
-    
-        // logic
-        for (int i=nowNum; i<N; i++) {
+
+        for (int i=index; i<N; i++) {
             if (isVisited[i]) continue;
-            
+
             isVisited[i] = true;
             arr[depth] = i+1;
-            dfs(i+1, depth+1);
+            sol(N, M, depth+1, i+1);
             isVisited[i] = false;
         }
     }
