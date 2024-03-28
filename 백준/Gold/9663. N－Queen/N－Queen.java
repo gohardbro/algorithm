bb1e675660c98af827591ef9d278b07e68656553
gohardbro <89912930+gohardbro@users.noreply.github.com>
2024-03-28 22:31:42 +0900

    [Gold IV] Title: N-Queen, Time: 5216 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,60 +1,44 @@
-/*
-1. 아이디어
-    - dfs 이용
-    - 한 행이나 열에서 놓을수있는 칸에 체스놓기 재귀호출 반복
-    - 종료조건: depth == N (즉, 체스를 다놓았을때)
-    - 마지막 체스를 놓으면 카운트
-
-2. 시간복잡도
-    - n * n^n+1
-
-3. 자료구조
-    - 체스위치를 저장할 int[] : index = 행, value = 열
-*/
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static int N;
-    private static int[] chessArr;
+    private static int[] chessIndex;
     private static int cnt = 0;
-    
-    public static void main(String args[]) throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
-        chessArr = new int[N];
-        
-        dfs(0);
-        
+        final int N = Integer.parseInt(br.readLine());
+        chessIndex = new int[N];
+
+        sol(N);
         System.out.println(cnt);
     }
-    
-    private static void dfs(int depth) {
-        // 종료조건
-        if (depth == N) {
+
+    private static void sol(int N) {
+        sol(N, 0);
+    }
+
+    private static void sol(int N, int row) {
+        if (row == N) {
             cnt++;
             return;
         }
-        
-        // logic
+
         for (int i=0; i<N; i++) {
-            chessArr[depth] = i;
-            
-            if (isPossible(depth)) {
-                dfs(depth + 1);
-            }
+            chessIndex[row] = i;
+
+            if (isPossible(row))
+                sol(N, row+1);
         }
     }
-    
-    // 한 열에서 체스놓을수있는 칸인지 boolean 으로 반환하는 메소드
-    private static boolean isPossible(int col) {
-        for (int i=0; i<col; i++) {
-            // 같은 행 제외 (십자 제외)
-            if (chessArr[col] == chessArr[i]) return false;
-            // 대각 제외
-            if (Math.abs(col-i) == Math.abs(chessArr[col] - chessArr[i])) return false;
+
+    private static boolean isPossible(int row) {
+        for (int i=0; i<row; i++) {
+            if (chessIndex[i] == chessIndex[row]) return false; // 십자 비교
+            if (Math.abs(row - i) == Math.abs(chessIndex[row] - chessIndex[i])) return false; // 대각선 비교
         }
-        
+
         return true;
     }
 }
