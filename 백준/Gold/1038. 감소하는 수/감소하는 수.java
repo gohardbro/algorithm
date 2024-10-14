7c718539e1a7f7e5c8fb724117921dde3dbdda98
gohardbro <89912930+gohardbro@users.noreply.github.com>
2024-10-14 10:08:21 +0900

    [Gold V] Title: 감소하는 수, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,35 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    private static int N;
-    private static List<Long> list = new ArrayList<>();
-    
-    public static void main(String args[]) throws IOException {
+    static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
         
-        N = Integer.parseInt(br.readLine());
-        
-        if (N < 11) {
+        if (N <= 10) {
             System.out.println(N);
-        } else if (N > 1022) {
-            System.out.println(-1);
         } else {
-            for (int i=0; i<10; i++) {
-                sol(i, 0);
+            for (int i=0; i<=9; i++){
+                addNum(i, 0);
             }
-            
-            Collections.sort(list);
-            System.out.println(list.get(N));
+
+            if (N >= list.size()) System.out.println(-1);
+            else {
+                Collections.sort(list);
+                System.out.println(list.get(N));
+            }
         }
     }
-    
-    private static void sol(long num, int depth) {
-        if (depth == 10) return;
+
+    private static void addNum(long num, int depth) {
+        if(depth == 10) return;
         list.add(num);
-        
-        for (int i=0; i<num % 10; i++) {
-            sol(num * 10 + i, depth + 1);
+
+        for (int i=0; i<num%10; i++) {
+            addNum(num*10+i, depth+1);
         }
-    }
+    }   
 }
